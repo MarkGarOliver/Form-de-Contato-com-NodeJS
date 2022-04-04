6e869cc9eea442fa761fdc148068dbe1e88338a4
MarkGarOliver <marcosgarcia.oliveira02@gmail.com>
2022-04-04 09:43:26 -0300

    add configs na linha 13 do handlebars para funcionar a consulta no banco de dados | deletado arq html de contato, agora renderizado com o handlebars | rota / agora esta passando os dados da consulta ao db para o arq home que esta sendo renderizado nessa rota

--- index.js
+++ index.js
@@ -10,34 +10,48 @@ const handlebars = require('express-handlebars')
         app.use(bodyparser.urlencoded({extended: false}))
         app.use(bodyparser.json())
     //handlebars
-        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main', runtimeOptions: {
+            allowProtoMethodsByDefault: true,
+
+            allowProtoPropertiesByDefault: true
+        }}))
         app.set('view engine', 'handlebars')
         app.set('views', './views')
 
 
 //Rotas
+    
+    /*
+    Na rota / pegamos todos os dados vindos de Contato(dados do DB) e com o then atribuimos isso a contato(parametro passado na função then), e na renderização, passamos todos esses dados com o nome contatos
 
-    app.get('/handlebars', (req, res) => {
-        res.render('home')
+    obs: dentro da função findAll, foi passado o Order para ordenar como decrescente, exibindo no mais novo para o mais antigo
+    */
+    app.get('/', (req, res) => {
+        Contato.findAll({order: [['id', 'DESC']]}).then(function(contato){
+            res.render('home', {contatos: contato})
+        })
+        
     })
 
+    //rota do form de contato
     app.get("/contato", function(req, res){
-        res.sendFile(__dirname + "/form/index.html")
+        res.render("formulario")
     })  
 
 
-    //recebe dados do form
+    //rota que recebe dados do form, e com esses dados criamos o registro no database
     app.post("/add", function(req, res){
         Contato.create({ //cria o registro no banco de dados das informações de contato
             nome: req.body.nome,
             email: req.body.email,
             mensagem: req.body.mensagem
         }).then(function(){ //Caso seja concluido com sucesso exibe essa mensagem
-            res.send('mensagem enviada com sucesso !')
+            res.redirect('/')
         }).catch(function(erro){ //caso ocorra um erro ele irá exibir o erro 
             res.send('Houve um erro: ' + erro)
         })
     })
+
 
 
 
